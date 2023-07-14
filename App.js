7b9e5a7d1e5698f952cc97443bd65f376b0ff1f1
Rachitha J R <rachithajr1412@gmail.com>
2023-07-14 21:59:22 +0530

    React Element and React Component

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//React.CreateElement=> JS object => render(JS object)=> HTML 
-const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])])
-// console.log(parent);
 
-//JSX=>Babel transpiles JSX to React.CreateElement React Object=> React.CreateElement=> JS object => render(JS object)=> HTML
-const jsxHeading=<h1 className="head">
+//React Element
+//To convert React Element to React Component make arrow function and capitalize
+const jsxHeading=(<h1 className="head" tabIndex="5">
   Namaste React from JSX
-  </h1>
-const root=ReactDOM.createRoot(document.getElementById("root"));
+  </h1>);
 root.render(jsxHeading);
 
-// root.render(<h1 className="head">
-// This is same as giving JSX object directly
-// </h1>);
-// console.log(jsxHeading);
+//React Component
+//should always be in capital letter
+//For rendering React component use <ComponentName/>
+//with return
+const ReactComponent=()=>{return <h1> Namaste React from React Functional Component</h1>}
+
+
+//without return and {}
+const ReactComponent2=()=><h1>React functional Component without Return keyword</h1>
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ReactComponent2/>)

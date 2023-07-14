b35255c943a252d4b3c730097bfb1a5c43707949
Rachitha J R <rachithajr1412@gmail.com>
2023-07-14 22:16:05 +0530

    Component Composition

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Element
-//To convert React Element to React Component make arrow function and capitalize
-const jsxHeading=(<h1 className="head" tabIndex="5">
-  Namaste React from JSX
-  </h1>);
-root.render(jsxHeading);
+//Component Composition- One Component used inside the other
+const Title=()=>(<h1>Namaste React using JSX</h1>);
+const HeadingComponent=()=>(
+<div>
+<Title/>
+<h1>Namaste React Using Functional Component</h1>
+</div>);
 
-//React Component
-//should always be in capital letter
-//For rendering React component use <ComponentName/>
-//with return
-const ReactComponent=()=>{return <h1> Namaste React from React Functional Component</h1>}
-
-
-//without return and {}
-const ReactComponent2=()=><h1>React functional Component without Return keyword</h1>
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ReactComponent2/>)
+root.render(<HeadingComponent/>)
+
+
+
+

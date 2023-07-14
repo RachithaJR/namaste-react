4243e3a57f3497cf8b8443a672759a8474fc16f7
Rachitha J R <rachithajr1412@gmail.com>
2023-07-14 13:47:43 +0530

    Episode 3.2

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-/*
-<div id="parent">
-<div id="child1">
-<h1>I am h1 tag</h1>
-<h2>I am h2 tag</h2>
-</div>
-<div id="child2">
-<h1>I am h1 tag</h1>
-<h2>I am h2 tag</h2>
-</div>
-</div>
-*/ 
 import React from "react";
 import ReactDOM from "react-dom/client";
+//React.CreateElement=> JS object => render(JS object)=> HTML 
 const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child1"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),React.createElement("div",{id:"child2"},[React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")])])
+// console.log(parent);
+
+//JSX=>Babel transpiles JSX to React.CreateElement React Object=> React.CreateElement=> JS object => render(JS object)=> HTML
+const jsxHeading=<h1 className="head">
+  Namaste React from JSX
+  </h1>
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
+
+// root.render(<h1 className="head">
+// This is same as giving JSX object directly
+// </h1>);
+// console.log(jsxHeading);

8e539c491515e634af9676b3315335ccc5f0e444
Rachitha J R <rachithajr1412@gmail.com>
2023-08-05 22:02:38 +0530

    React Element and Component inside each other

--- App.js
+++ App.js
@@ -1,16 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Component Composition- One Component used inside the other
-const Title=()=>(<h1>Namaste React using JSX</h1>);
-const HeadingComponent=()=>(
-<div>
-<Title/>
-<h1>Namaste React Using Functional Component</h1>
-</div>);
+//React Element used inside Component
+// const title=<h1>Namaste React using JSX</h1>;
+// const HeadingComponent=()=>(
+// <div>
+// {title}
+// <h1>Namaste React Using Functional Component</h1>
+// </div>);
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingComponent/>)
+
+//React Element inside React Element
+// const title=<h1>Namaste React using JSX</h1>;
+// const headingComponent=(
+// <div>
+// {title}
+// <h1>Namaste React Using Functional Component</h1>
+// </div>);
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(headingComponent)
+
+//React Component inside React Element
+const HeadingComponent=()=>(<div>
+  <h1>Namaste React Using Functional Component</h1>
+  </div>);
+const title=<h1><HeadingComponent/>Namaste React using JSX</h1>;
+
+
+
+//infinite loop title and HeadingComponent is used insid eeach other
+//if react element used before defining then inifinite loop
+// const HeadingComponent=()=>(<div>
+//   {title}
+//   <h1>Namaste React Using Functional Component</h1>
+//   </div>);
+// const title=<h1><HeadingComponent/>Namaste React using JSX</h1>;
+
+//if component is used before defining then results in error
+// ReferenceError: Cannot access 'HeadingComponent' before initialization
+
+// const title=<h1><HeadingComponent/>Namaste React using JSX</h1>;
+// const HeadingComponent=()=>(<div>
+//   {title}
+//   <h1>Namaste React Using Functional Component</h1>
+//   </div>);
+
+
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>)
+root.render(title)
 
 
 

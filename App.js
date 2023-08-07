604cce150125cf1a59b285c36164ac2944efb833
Rachitha J R <rachithajr1412@gmail.com>
2023-08-07 09:52:32 +0530

    React Fragment as single parent in JSX

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const number=100;
-const Header=()=>(<h1>{number+ 10}Hello I am React Functional Component
-{console.log("My name is Rachitha")}</h1>)
+const Header=()=>(<React.Fragment><h1>{0 +1} Hello I am React Functional Component
+{console.log("My name is Rachitha")}</h1><div> JSX can have single parent</div></React.Fragment>)
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Header/>)
